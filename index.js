a43af78ff17bb408c9f390171a1be560a57ab1dc
adrifoz <adrianofadel7@gmail.com>
2024-07-24 00:22:03 -0300

    versión entrega 1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const productos = [
-    "Caño Agua Termofusión 1/2",
-    "Caño Agua Termofusión 3/4",
-    "Caño Agua Termofusión 1",
-    "Caño Gas Termofusión 1/2",
-    "Caño Gas Termofusión 3/4",
-    "Caño Gas Termofusión 1"
+  "1. Caño Agua Termofusión 1/2",
+  "2. Caño Agua Termofusión 3/4",
+  "3. Caño Agua Termofusión 1",
+  "4. Caño Gas Termofusión 1/2",
+  "5. Caño Gas Termofusión 3/4",
+  "6. Caño Gas Termofusión 1"
 ];
+
+const cantidades = [];
 
 const carrito = [];
 
@@ -17,14 +19,19 @@ let comprobacion = carrito.includes(nombre);
 
 // FUNCIONES COMPLEMENTARIAS
 
+
+
 function finalizarCompra() {
 
-    const retorno = confirm("¿Desea continuar comprando?");
-    if (total !== 0 && retorno) {
+    const pagar = confirm("¿Desea efectuar el pago?");
+    if (total !== 0 && pagar) {
        
-        confirm("El total es $" + total + "\n\n" + "Producto/s:\n\n" + carrito.join("  X1 \n") + "  X1" + "\n\n" +"¿Desea confirmar la compra?");
+        confirm("El total es $" + total + "\n\n" + "Historial de compra" + "\n\n" + carrito.join("\n") +"\n\n" +"¿Desea confirmar la compra?");
+        alert("Compra realizada con éxito");
         alert("Gracias por visitarnos.");
-    }else if(retorno){
+        total = 0;
+        menu();
+    }else if(pagar){
         alert("Todavía no tiene nada en su carrito")
         menu();
     }else{
@@ -38,12 +45,10 @@ function finalizarCompra() {
       total += precioTotal
     }
 
-    function addCarrito(nombre){
-if (!comprobacion){
-        {console.log(carrito)}
+    function addCarrito(nombre,cantidad){
+      carrito.push(cantidad)
       carrito.push(nombre)
-      {console.log(carrito)}
-}
+      
     }
 
     function mostrarProductos() {
@@ -59,42 +64,42 @@ if (!comprobacion){
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 8000;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 2:
                 nombre = "Caño Agua Termofusión 3/4";
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 9500;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 3:
                 nombre = "Caño Agua Termofusión 1";
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 11000;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 4:
                 nombre = "Caño Gas Termofusión 1/2";
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 12000;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 5:
                 nombre = "Caño Gas Termofusión 3/4";
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 15000;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 6:
                 nombre = "Caño Gas Termofusión 1";
                 cantidad = parseInt(prompt(nombre +"\n\n"+ "Seleccione cuantos"));
                 cantidadXvalor = cantidad * 18000;
                 suma(cantidadXvalor);
-                addCarrito(nombre);
+                addCarrito(nombre,cantidad);
               break;
             case 7:
                 menu();
